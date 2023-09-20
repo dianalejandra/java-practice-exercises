f9750bc4a0dd15496fc6a63b2e73e21908324776
abatediana <diana.abate@gmail.com>
2023-09-20 12:56:06 -0300

    Completed christmas tree exercice

--- src/main/java/dianapc/ChristmasTree.java
+++ src/main/java/dianapc/ChristmasTree.java
@@ -10,6 +10,36 @@ public class ChristmasTree {
     
     public static void main (String[] args){
         
+        int height = 8;
+        
+        for(int row = 0; row < height; row++ ){
+            
+            for(int space = 0; space < (height-row-1); space++){
+                
+                System.out.print(" ");
+            }
+            
+            for (int asterisk = 0; asterisk < (row*2)+1; asterisk++){
+                
+                System.out.print("*");
+                
+            }
+            System.out.println("");
+            
+        }
+        
+        int longTrunk = 1;
+        for(int base=0; base<longTrunk;base++){
+            
+            for(int space=0; space<(height-2);space++){
+                System.out.print(" ");            
+            }
+            
+            for(int trunk=0; trunk<3; trunk++){
+                System.out.print("|");
+            }
+            
+            System.out.println("");
+        }
     }
-    
 }
